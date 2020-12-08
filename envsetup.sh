a7b61525e2384036b619cb10bd3c00c36c8fff73
abun880007 <abun880007@msn.com>
2020-12-08 03:07:28 +0000

    Update AOSP to 11.0.0 r21

--- envsetup.sh
+++ envsetup.sh
@@ -598,7 +598,7 @@ function print_lunch_menu()
     tput bold;
     echo ""
     echo "************************************************"
-    echo "        420om - Android base 11.0.0 r41         "
+    echo "        420om - Android base 11.0.0 r21         "
     echo "************************************************"
     echo ""
     echo "You're building on" $uname
