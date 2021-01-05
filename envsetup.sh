45b5fae01fda53bb8b6076d545bccc15e9cddb6a
abun880007 <abun880007@msn.com>
2021-01-05 11:32:06 +0000

    Merge tag 'android-11.0.0_r28' into build.
    # Conflicts:
    #	core/build_id.mk

--- envsetup.sh
+++ envsetup.sh
@@ -598,7 +598,7 @@ function print_lunch_menu()
     tput bold;
     echo ""
     echo "************************************************"
-    echo "        420om - Android base 11.0.0 r24         "
+    echo "        420om - Android base 11.0.0 r28         "
     echo "************************************************"
     echo ""
     echo "You're building on" $uname
