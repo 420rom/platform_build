40eed8d1384f6c5ccff470c2beb9c34fe7b3168d
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2021-05-30 16:43:02 +0200

    envsetup.sh: Add support for aosp prefix
    This change allows dotOS to also
    build using aosp_ prefix for product.
    
    Change-Id: Ic6e0789d57f87e2994c87a66d572186e97af0e5d

--- envsetup.sh
+++ envsetup.sh
@@ -141,7 +141,11 @@ function check_product()
     if (echo -n $1 | grep -q -e "^Four20rom_") ; then
         DOT_BUILD=$(echo -n $1 | sed -e 's/^Four20rom_//g')
     else
-        DOT_BUILD=
+        if (echo -n $1 | grep -q -e "^aosp_") ; then
+            DOT_BUILD=$(echo -n $1 | sed -e 's/^aosp_//g')
+        else
+            DOT_BUILD=
+        fi
     fi
     export DOT_BUILD
 
